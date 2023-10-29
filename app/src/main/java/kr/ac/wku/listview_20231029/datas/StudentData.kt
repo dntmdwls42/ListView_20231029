988f0ace2249d2bdaf579b01b764d6975387e0d6
dntmdwls42 <dntmdwls42@wku.ac.kr>
2023-10-29 11:28:08 +0900

    학생 데이터 + xml 조합해서 뿌려줄 클래스 StudentAdapter 생성 -ArrayAdapter 상속 => 기본생성자 지원 안함 -어댑터클래스가 생성자에서 변수 받아오게 -받아오는 변수를 부모클래스에 전달

---
+++ app/src/main/java/kr/ac/wku/listview_20231029/datas/StudentData.kt
@@ -0,0 +1,8 @@
+package kr.ac.wku.listview_20231029.datas
+
+class StudentData ( //studentData의 생성자 변경 :  이름, 출생년도, 폰번을 넣어서 생성
+    name: String,
+    birthYear: Int,
+    phoneNum: String ){
+    
+}
